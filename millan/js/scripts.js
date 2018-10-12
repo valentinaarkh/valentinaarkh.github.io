c769fa4be98fa2497c01264f224b1717c4e9305e
Valentina <arkhutichvalya1@gmail.com>
2018-10-13 00:43:30 +0300

    Menu animation and CDN change
    Add menu animations and change CDN links to downloaded assets

--- millan/js/scripts.js
+++ millan/js/scripts.js
@@ -1,3 +1,43 @@
+$(document).ready(function(){
+
+// Smooth menu anumation
+
+$(".main_menu").on("click", "a", function(event){
+  event.preventDefault();
+
+  let id = $(this).attr("href");
+  let top = $(id).offset().top;
+ $("body, html").animate({
+   scrollTop: top
+ }, 500);
+});
+
+// Small device menu animation
+
+$(".burger_menu").click(function(){
+  $(this).toggleClass("is_open");
+  $(".main_menu_wrapper").toggleClass("visible");
+});
+
+// Button top animation
+let top_button = $(".top_button");
+
+$(window).scroll(function(){
+  if ($(window).scrollTop() > "400" ){
+    top_button.fadeIn();
+  }else{
+    top_button.fadeOut();
+  }
+});
+
+top_button.click(function(){
+  $("body, html").animate({
+    scrollTop: 0
+  }, 500);
+});
+
+// Gallery animation
+
 $('.gallery_slider_large').slick({
  slidesToShow: 1,
  slidesToScroll: 1,
@@ -14,7 +54,5 @@ $('.gallery_slider').slick({
  focusOnSelect: true
 });
 
-$(".burger_menu").click(function(){
-  $(this).toggleClass("is_open");
-  $(".main_menu_wrapper").toggleClass("visible");
+
 });
